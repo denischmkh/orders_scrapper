d3a37e66d33e3e2dfd31b3c28d721f7eac48cca9
denischmkh <avraam.l2004@gmail.com>
2025-02-27 17:50:45 +0200

    fix bugs with time

--- src/main.py
+++ src/main.py
@@ -45,18 +45,12 @@ async def handler(event):
         if "нужны грузчики" in message.text.lower() and 'кто первый поставит “+“' in message.text.lower():
             # Отправляем ответ на сообщение
             await message.reply("+")
-
             sender = True
         elif "нужны грузчики" in message.text.lower() and 'напишите когда вы сможете быть на заказе' in message.text.lower():
             kyiv_tz = pytz.timezone('Europe/Kiev')
             time_now = datetime.datetime.now(kyiv_tz)
-            minutes = time_now.minute
-            rounded_minutes = 5 * round(minutes / 5) + 20
-            if rounded_minutes == 60:
-                time_now += datetime.timedelta(hours=1)
-                rounded_minutes = 0
-            time_in_rounded = time_now.replace(minute=rounded_minutes, second=0, microsecond=0)
-            time_str = time_in_rounded.strftime('%H:%M')
+            time_in_20_minutes = time_now + datetime.timedelta(minutes=(20 + (10 - time_now.minute % 10)))
+            time_str = time_in_20_minutes.strftime('%H:%M')
             await message.reply(f"{time_str}")
             sender = True
     else:
@@ -71,6 +65,7 @@ async def handler2(event):
     if fishing_active and with_partner_fishing:
         await asyncio.sleep(1)
         message = event.message
+        # Проверка, если в тексте сообщения содержится "Нужны грузчики"
         if "нужны грузчики" in message.text.lower() and 'кто первый поставит “+“' in message.text.lower():
             # Отправляем ответ на сообщение
             await message.reply("+")
@@ -78,13 +73,8 @@ async def handler2(event):
         elif "нужны грузчики" in message.text.lower() and 'напишите когда вы сможете быть на заказе' in message.text.lower():
             kyiv_tz = pytz.timezone('Europe/Kiev')
             time_now = datetime.datetime.now(kyiv_tz)
-            minutes = time_now.minute
-            rounded_minutes = 5 * round(minutes / 5) + 20
-            if rounded_minutes == 60:
-                time_now += datetime.timedelta(hours=1)
-                rounded_minutes = 0
-            time_in_rounded = time_now.replace(minute=rounded_minutes, second=0, microsecond=0)
-            time_str = time_in_rounded.strftime('%H:%M')
+            time_in_20_minutes = time_now + datetime.timedelta(minutes=(20 + (10 - time_now.minute % 10)))
+            time_str = time_in_20_minutes.strftime('%H:%M')
             await message.reply(f"{time_str}")
             sender = True
     else:
